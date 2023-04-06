8f9a8c0c1b775fbc21ebf9e8d0d54a58595de831
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-06 19:38:48 +0800

    Add files via upload

---
+++ javacode/day2/Demo3.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// 遞增++, 遞減-- 運算子
+		int age = 10;
+		//age = age + 1;
+		//age++;
+		//age = age - 1;
+		age--;
+		System.out.println(age);
+	}
+
+}
