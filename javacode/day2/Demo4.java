e92f4c4dbcb43153b9f440f8f142d30c0d536aa4
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-06 19:55:52 +0800

    Add files via upload

---
+++ javacode/day2/Demo4.java
@@ -0,0 +1,14 @@
+package day2;
+
+public class Demo4 {
+	public static void main(String[] args) {
+		
+		// 變數與 ++, -- 的應用
+		// 前序遞增, 後序遞增
+		// 前序遞減, 後序遞減
+		int age = 10;
+		int var = age++;
+		System.out.println(age);
+		System.out.println(var);
+	}
+}
