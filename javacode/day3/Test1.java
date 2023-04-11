f8310aa9ed74fcbde77dc4f05c1668167610fdff
VincentTuan <vincentjava@yahoo.com.tw>
2023-04-11 19:29:02 +0800

    Add files via upload

---
+++ javacode/day3/Test1.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Test1 {
+	public static void main(String[] args) {
+		// Java 基本資料型別-整數
+		byte  x = 10; // byte 資料範圍: -128 ~ 127
+		short y = 28000; // short 資料範圍: -3.2萬 ~ 3.2 萬
+		int   z = 88000; // int 資料範圍: -21億 ~ 21億
+		long  m = 999999999; // long 資料範圍: -900京 ~ 900京
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(m);
+	}
+}
